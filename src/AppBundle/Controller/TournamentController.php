a32cfba710c0d43caa97f78ee515dd95af17bffd
Paul Novikov <paulfcdd@gmail.com>
2017-03-18 19:46:11 +0100

    rewrite admin lte template and some changes in logic

--- src/AppBundle/Controller/TournamentController.php
+++ src/AppBundle/Controller/TournamentController.php
@@ -8,9 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class TournamentController extends Controller
 {
     /**
-     * @Route("/tournament/list", name="site.tournament.list")
+     * @Route("/tournament", name="site.tournament")
      */
     public function tournamentListAction(){
-        return $this->render(':app/tournament:list.html.twig');
+        return $this->render(':app:tournament.html.twig');
     }
 }
